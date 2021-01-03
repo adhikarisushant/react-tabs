4e765f513ab6a2985ada09f90bfacf81d0ec5a4c
Sushant <sushant.adhikari.619@gmail.com>
2021-01-03 21:10:40 +0545

    projecct completed

--- src/App.js
+++ src/App.js
@@ -26,7 +26,47 @@ function App() {
     );
   }
 
-  return <h2>tabs project setup</h2>
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+          <h2>experience</h2>
+          <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+
+        {/* BTN CONTAINER */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button 
+              key={item.id}
+              onClick={() => setValue(index)}
+              className={`job-btn ${index === value && 'active-btn'}`}
+              >{item.company}</button>
+            );
+          })}
+        </div>
+
+
+        {/* JOB INFO */}
+        <article className="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty, index)=>{
+                return (
+                  <div key={index} className="job-desc">
+                      <FaAngleDoubleRight className="job-icon" />
+                      <p>{duty}</p>
+                  </div>
+                );
+            })};
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App
